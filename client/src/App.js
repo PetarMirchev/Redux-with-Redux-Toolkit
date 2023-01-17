414baa5b3bb3d452c911b54e1edf3af22185270c
Petar Mirchev <petar.g.mirchev@gmail.com>
2023-01-17 21:15:39 +0200

    create Navbar & install MUI
    create some Navbar for the test

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hello test !</h1>
+    <div className="App"> 
+      <Navbar/>
     </div>
   );
 }
---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import "./navbar.css";
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="navbarWrapper">
+        <div className="navbarLeft">
+          <span className='logo'>Pepi App</span>
+          <span className='navbarLink'>Home</span>
+          <span className='navbarLink'>About</span>
+          <span className='navbarLink'>Contact</span>
+        </div>
+      <div className="navbarCenter">
+        <div className="search">
+          <input
+            type="text"
+            placeholder='search......'
+            className='searchInput'
+          />
+        </div>
+      </div>
+        <div className="navbarRight">
+          <img
+            className='avatar'
+            src="https://images.pexels.com/photos/3024627/pexels-photo-3024627.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=''
+          />
+          <span className='navbarName'>Name X</span>
+          <ArrowDownwardIcon/>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Navbar;
